ad4ca972a075a58d024a85159c129d62d05c7612
Georg Müller <georg@thingsthinking.net>
2022-11-04 10:07:58 +0100

    added 2 more sample sentences

--- semantha/st/pages/compare_page.py
+++ semantha/st/pages/compare_page.py
@@ -3,8 +3,10 @@ import streamlit as st
 from semantha.st.components.compare import SemanticCompare
 
 example_sentences = {
+    "newspaper": ["The vehicle was too fast.", "The car was going at excessive speed."],
     "fruits": ["I like to eat apples.", "I fancy fruit."],
     "multi language": ["Bananen schmecken mir.", "I like banana."],
+    "job": ["I work for Microsoft.", "My job is in a software company."]
 }
 
 
