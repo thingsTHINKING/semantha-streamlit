21f699fd53e6130c7fb0a537eaa8f0d8076efb5b
Georg Müller <georg@thingsthinking.net>
2022-11-04 11:37:18 +0100

    better texts and examples

--- semantha/st/components/compare.py
+++ semantha/st/components/compare.py
@@ -5,26 +5,37 @@ from semantha.st.components.semantha import Semantha
 
 
 class SemanticCompare:
+
+    __semantha_connector: Semantha
+    __compare_domain: str
+    
+    __first_text = ""
+    __second_text = ""
+
     def __init__(
         self,
         server_url: str = st.secrets["semantha"]["server_url"],
         api_key: str = st.secrets["semantha"]["api_key"],
-        domain: str = st.secrets["semantha"]["domain"],
-        sentences: Optional[Tuple[str, str]] = ["", ""],
+        domain: str = st.secrets["semantha"]["domain"]
+        
     ):
-        self._semantha_connector = Semantha(server_url, api_key)
+        self.__semantha_connector = Semantha(server_url, api_key)
         self.__compare_domain = domain
-        self.__first_text = sentences[0]
-        self.__second_text = sentences[1]
 
-    def build(self, md_title = "## Direct Compare", description = "Directly compare two texts by entering them below. The texts will be compared using semantha's® semantic model."):
-        st.markdown(md_title)
-        st.write(description)
+    def build(self):
+        self.build_title()
         self.build_input()
 
+    def build_title(self, md_title="Semantic Comparison", description="Directly compare two sentences by entering them below. The sentences will be compared using semantha's® semantic model."):
+        st.title(md_title)
+        st.write(description)
+        
     def build_input(
-        self, label_1="Input I", label_2="Input II", button="⇆ Semantic Compare", spinner_msg = "Wait for it..."
+        self, label_1="Sentence I", label_2="Sentence II", button="⇆ Compare", spinner_msg = "Wait for it...", 
+        sentences: Optional[Tuple[str, str]] = ["", ""]
     ):
+        self.__first_text = sentences[0]
+        self.__second_text = sentences[1]
         input_0 = st.text_input(label=label_1, value=self.__first_text)
         input_1 = st.text_input(label=label_2, value=self.__second_text)
         _, col, _ = st.columns(3)
@@ -33,17 +44,22 @@ class SemanticCompare:
                 self.compute_and_display_similarity(input_0, input_1)
 
     def compute_and_display_similarity(self, input_0: str, input_1: str):
-        similarity, contradictory = self._semantha_connector.compare_with_omd(
+        similarity, contradictory = self.__semantha_connector.compare_with_omd(
             input_0, input_1, self.__compare_domain
         )
         sim = int(round(similarity, 2) * 100)
-        additional_text = " but opposite meaning." if contradictory else "."
 
         if sim > 99:
-            st.markdown(f"### 🦸🏼‍♀️ IDENTICAL{additional_text}")
-        elif sim <= 99 and sim > 60:
-            st.markdown(f"### 🦸🏼‍♀️ SIMILAR{additional_text}")
-        elif sim <= 60 and sim > 40:
-            st.markdown("### 🦸🏼‍♀️ Small connection between sentences")
+            if contradictory:
+                st.warning("⚠️ Texts are identical but have OPPOSITE meaning.")
+            else:
+                st.success("🕺 Texts are identical")
+        elif 99 >= sim > 60:
+            if contradictory:
+                st.warning("⚠️ Texts are similar but have OPPOSITE meaning.")
+            else:
+                st.success("🕺 Texts are similar")
+        elif 60 >= sim > 40:
+            st.warning("🙋 Small connection between sentences.")
         elif sim <= 40:
-            st.markdown("### 🦸🏼‍♀️ Not similar")
+            st.error("🙅‍♂️ Not similar.")
--- semantha/st/pages/compare_page.py
+++ semantha/st/pages/compare_page.py
@@ -3,15 +3,18 @@ import streamlit as st
 from semantha.st.components.compare import SemanticCompare
 
 example_sentences = {
-    "newspaper": ["The vehicle was too fast.", "The car was going at excessive speed."],
     "fruits": ["I like to eat apples.", "I fancy fruit."],
     "multi language": ["Bananen schmecken mir.", "I like banana."],
-    "job": ["I work for Microsoft.", "My job is in a software company."]
+    "newspaper": ["The vehicle was too fast.", "The car was going at excessive speed."],
+    "job": ["I work for Microsoft.", "My job is in a software company."],
+    "opposite meaning": ["I got hired.", "I got fired."], 
+    "opposite meaning 2": ["These apples are damn good.", "I hate these apples."]
 }
 
 
 class SemanticComparePage:
     def build(self):
-        option = st.selectbox("Select Example", ["fruits", "multi language"])
-        compare = SemanticCompare(sentences=example_sentences[option])
-        compare.build()
+        compare = SemanticCompare()
+        compare.build_title()
+        option = st.selectbox("Select Example", ["fruits", "multi language", "newspaper", "job", "opposite meaning", "opposite meaning 2"])
+        compare.build_input(sentences=example_sentences[option])
