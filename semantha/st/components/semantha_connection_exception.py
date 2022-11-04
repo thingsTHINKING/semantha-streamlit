21bb51b165052c298888a203c485aef69cfa7a2c
Georg Müller <georg@thingsthinking.net>
2022-11-04 14:55:59 +0100

    reuse component published on pypi

--- semantha/st/components/semantha_connection_exception.py
+++
@@ -1,2 +0,0 @@
-class SemanthaConnectionException(Exception):
-    """SemanthaConnectionException"""
--- semantha/st/pages/compare_page.py
+++ semantha/st/pages/compare_page.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
-from semantha.st.components.compare import SemanticCompare
+from semantha_streamlit_compare.components.compare import SemanticCompare
 
 example_sentences = {
     "fruits": ["I like to eat apples.", "I fancy fruit."],
     "multi language": ["Bananen schmecken mir.", "I like banana."],
     "newspaper": ["The vehicle was too fast.", "The car was going at excessive speed."],
     "job": ["I work for Microsoft.", "My job is in a software company."],
-    "opposite meaning": ["I got hired.", "I got fired."], 
-    "opposite meaning 2": ["These apples are damn good.", "I hate these apples."]
+    "opposite meaning": ["I got hired.", "I got fired."],
+    "opposite meaning 2": ["These apples are damn good.", "I hate these apples."],
 }
 
 
@@ -16,5 +16,15 @@ class SemanticComparePage:
     def build(self):
         compare = SemanticCompare()
         compare.build_title()
-        option = st.selectbox("Select Example", ["fruits", "multi language", "newspaper", "job", "opposite meaning", "opposite meaning 2"])
+        option = st.selectbox(
+            "Select Example",
+            [
+                "fruits",
+                "multi language",
+                "newspaper",
+                "job",
+                "opposite meaning",
+                "opposite meaning 2",
+            ],
+        )
         compare.build_input(sentences=example_sentences[option])
